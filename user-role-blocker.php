cd48094dfb0c7bf5039e550b6f30f9e16fde13a1
Hasin Hayder <countdraculla@gmail.com>
2020-04-21 22:36:10 +0600

    Cleanup

--- user-role-blocker.php
+++ user-role-blocker.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: User Role Blocker
-Plugin URI:
-Description: A simple and nice plugin to block existing users from logging into the admin panel by assigning them to 'Blocked' user role, as simple as that. 
+Plugin URI: https://github.com/LearnWithHasinHayder/user-role-blocker
+Description: A simple and nice plugin to block existing users from logging into the admin panel by assigning them to 'Blocked' user role, as simple as that.
 Version: 1.0.0
 Author: Captain Haddock
 Author URI: mailto:captainhaddock@leevio.com
@@ -10,38 +10,38 @@ License: GPLv2 or later
 Text Domain: user-role-blocker
  */
 add_action( 'init', function () {
-    add_role( 'urb_user_blocked', __( 'Blocked', 'user-role-blocker' ), array('blocked' => true) );
-    add_rewrite_rule('blocked/?$','index.php?blocked=1','top');
+    add_role( 'urb_user_blocked', __( 'Blocked', 'user-role-blocker' ), array( 'blocked' => true ) );
+    add_rewrite_rule( 'blocked/?$', 'index.php?blocked=1', 'top' );
 } );
 
-add_action('init',function(){
-    if(is_admin() && current_user_can('blocked')){
-        wp_redirect(get_home_url().'/blocked');
+add_action( 'init', function () {
+    if ( is_admin() && current_user_can( 'blocked' ) ) {
+        wp_redirect( get_home_url() . '/blocked' );
         die();
     }
-});
+} );
 
-add_filter('query_vars',function($query_vars){
-    $query_vars[]='blocked';
+add_filter( 'query_vars', function ( $query_vars ) {
+    $query_vars[] = 'blocked';
     return $query_vars;
-});
+} );
 
-add_action('template_redirect',function(){
-    $is_blocked = intval(get_query_var('blocked'));
-    if($is_blocked){
+add_action( 'template_redirect', function () {
+    $is_blocked = intval( get_query_var( 'blocked' ) );
+    if ( $is_blocked ) {
         ?>
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title><?php _e('Blocked User','user-role-blocker') ;?></title>
+            <title><?php _e( 'Blocked User', 'user-role-blocker' );?></title>
         </head>
         <body>
-            <h2 style="text-align: center"><?php _e('You are blocked','user-role-blocker')  ;?></h2>
+            <h2 style="text-align: center"><?php _e( 'You are blocked', 'user-role-blocker' );?></h2>
         </body>
         </html>
         <?php
-        die();
+die();
     }
-}); 
+} );
